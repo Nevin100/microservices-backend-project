717eaf9b3b7350399b249358ed1dafab1ba8d3df
Nevin100 <opopopopop100000@gmail.com>
2026-07-11 20:46:50 +0530

    Completing Initialliziation Properly

---
+++ services/auth-service/src/index.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
+
+const app = express();
+const PORT = process.env.PORT;
+
+
+app.use(helmet());
+app.use(express.json());
+app.use(cors())
+
+app.listen(PORT , () => {
+    
+})
